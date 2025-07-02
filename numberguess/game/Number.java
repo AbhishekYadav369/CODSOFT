91d3d4ae5ba411c7c81a7cc52cb723df74ca6b0a
YADAV ABHISHEK JAGDISH <abhishekyadav2004max@gmail.com>
2025-07-02 15:07:55 +0530

    Number Guessing Game

--- numberguess/game/Number.java
+++ numberguess/game/Number.java
@@ -1,27 +1,31 @@
 package numberguess.game;
 import java.util.Scanner;
 public class Number {
+    static Guess  gues = new Guess();
+   private static int generateGuessNumber(Scanner scanner) {
+        System.out.print("Please guess the number between 1 and 100 :");
+        return scanner.nextInt();
+    }
     public static void main(String []args){
         // Generate a random number between 0 and 100
         int random = (int) (Math.random() * 101);
         System.out.println("Random number generated (for testing purposes): " + random);
         System.out.println("Welcome to the Number Guessing Game!");
-        System.out.print("Please guess the number between 1 and 100 :");
-      try( Scanner scanner = new Scanner(System.in)){
-          int guess = scanner.nextInt();
-        Guess gues = new Guess();
+       try( Scanner scanner = new Scanner(System.in)){
+          int guess ;
           boolean response = true;
-         while (!gues.guess(random, guess)&& response){
+          boolean guessed = false;
+         while (!guessed&& response){
             if (gues.getCount() >2) {
                 System.out.println("You have exceeded the maximum number of attempts :"+gues.getCount());
                 System.out.println("If you want to play again, please enter 'yes', otherwise enter 'no' to exit.");
                 response = scanner.next().equalsIgnoreCase("yes");
-                gues.setCount(-1);
+                gues.setCount(0);
                 gues.setScore(-2);
                 gues.setRound(1);
             }
-           else{  System.out.print("Please guess again:");
-             guess = scanner.nextInt();
+           else {  guess= generateGuessNumber(scanner);
+             guessed=gues.guess(random, guess);
            }
          }
          System.out.println("Your score is: " + gues.getScore());
